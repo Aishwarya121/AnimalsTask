f2dd59e2eea830c07b168ad24b762f554888a514
Aishwarya Salunke <aishwarya111salunke@gmail.com>
2021-10-11 15:03:16 +0530

    First commit

---
+++ Dashboard.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Online Voting Systeem</title>
+    </head>
+
+    <body>
+        <button>Back</button>
+        <button>Log Out</button>
+        <h1>ONLINE VOTING</h1>
+        <hr>
+    </body>
+</html>
